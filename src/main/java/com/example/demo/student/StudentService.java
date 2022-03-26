df62d6399b95b20cd718bb30efffc457778d8490
Icaro Peretti <icaroperetti50@gmail.com>
2022-03-26 15:19:31 -0300

    Saving student to database

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class StudentService {
@@ -19,5 +20,13 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail =  studentRepository.findStudentByEmail(student.getEmail());
+        if(studentByEmail.isPresent()) {
+            throw new IllegalStateException("E-mail taken");
+        }
+        studentRepository.save(student);
     }
 }
